57fd0dd51f3ffe617b0e51aa3f6614c645f0168a
Bernard Potocki <b.potocki@imanel.org>
2010-09-09 19:37:18 +0200

    Remove old syntax

--- init.rb
+++ init.rb
@@ -4,19 +4,3 @@ require File.dirname(__FILE__) + '/lib/socky/helper'
 ActionView::Base.send(:include, Socky::Helper)
 
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :socky => ['socky/swfobject', 'socky/FABridge', 'socky/web_socket', 'socky/json2', 'socky']
-
-if Rails::VERSION::MAJOR == 2
-  require File.dirname(__FILE__) + '/lib/socky/render_extension'
-
-  ActionController::Base.class_eval do
-    alias_method :render_without_socky, :render
-    include Socky::RenderExtension
-    alias_method :render, :render_with_socky
-  end
-
-  ActionView::Base.class_eval do
-    alias_method :render_without_socky, :render
-    include Socky::RenderExtension
-    alias_method :render, :render_with_socky
-  end
-end
--- lib/socky.rb
+++ lib/socky.rb
@@ -28,33 +28,6 @@ module Socky
       Rails.logger.warn "DEPRECATION WARNING: " + msg.to_s
     end
 
-    # Depricated methods
-
-    def send_to_all(data)
-      deprecation_warning "Using send_to_all is depricated and will be removed before stable version. Please use Socky.send instead."
-      send(data)
-    end
-
-    def send_to_clients(data, clients)
-      deprecation_warning "Using send_to_clients is depricated and will be removed before stable version. Please use Socky.send instead."
-      send(data, :to => {:clients => clients})
-    end
-    alias send_to_client send_to_clients
-
-    def send_to_channels(data, channels)
-      deprecation_warning "Using send_to_channels is depricated and will be removed before stable version. Please use Socky.send instead."
-      send(data, :to => {:channels => channels})
-    end
-    alias send_to_channel send_to_channels
-
-    def send_to_clients_on_channels(data, clients, channels)
-      deprecation_warning "Using send_to_clients_on_channels is depricated and will be removed before stable version. Please use Socky.send instead."
-      send(data, :to => {:clients => clients, :channels => channels})
-    end
-    alias send_to_clients_on_channel send_to_clients_on_channels
-    alias send_to_client_on_channels send_to_clients_on_channels
-    alias send_to_client_on_channel  send_to_clients_on_channels
-
   private
 
     def normalize_options(data = nil, options = {}, &block)
